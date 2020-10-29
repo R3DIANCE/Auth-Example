e22b41e80fa2f5e413523910164e901d3f3f5a2c
ZackaryH8 <zackary12389@yahoo.co.uk>
2020-10-29 20:09:43 +0000

    Added meta

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,44 +1,43 @@
 <template>
-  <div id="home">
-    <h1>Home</h1>
+    <div id="home">
+        <h1>Home</h1>
 
-    <img id="main-img" :src="require('@/assets/img/main.png')" />
+        <img id="main-img" :src="require('@/assets/img/main.png')" />
 
-    <p id="img-caption">
-      Rebel Roleplay is an upcoming survival Roleplay server for Grand Theft
-      Auto V, the focus will be both on combat and roleplay set in a post
-      apocalyptic Los Santos
-    </p>
-  </div>
+        <p id="img-caption">Rebel Roleplay is an upcoming survival Roleplay server for Grand Theft Auto V, the focus will be both on combat and roleplay set in a post apocalyptic Los Santos</p>
+    </div>
 </template>
 
 <script>
-import { mapState } from "vuex";
+import { mapState } from 'vuex';
 
 export default {
-  name: "Home",
+    name: 'Home',
+    metaInfo: {
+        title: `Rebel Roleplay - Home`,
+    },
 
-  computed: {
-    ...mapState(["auth"]),
-  },
+    computed: {
+        ...mapState(['auth']),
+    },
 };
 </script>
 
 <style scoped>
 #home {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
-
-#main-img {
-  max-width: 100%;
-  height: auto;
-  max-height: 100%;
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
 }
 
 #img-caption {
-  width: 90%;
+    width: 90%;
+}
+
+#main-img {
+    height: auto;
+    max-height: 100%;
+    max-width: 100%;
 }
 </style>
