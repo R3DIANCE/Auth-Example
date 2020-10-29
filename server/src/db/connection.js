4b500840d9846f2299454bba812426d917c386da
ZackaryH8 <zackary12389@yahoo.co.uk>
2020-10-29 20:05:41 +0000

    Prettier formatting

--- server/src/db/connection.js
+++ server/src/db/connection.js
@@ -3,7 +3,7 @@ const monk = require('monk');
 let dbUrl = process.env.DB_URL;
 
 if (process.env.NODE_ENV === 'test') {
-  dbUrl = process.env.TEST_DB_URL;
+    dbUrl = process.env.TEST_DB_URL;
 }
 
 const db = monk(dbUrl);
