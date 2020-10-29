c4b31ee5b542e05a185a043c151c6867b50521db
ZackaryH8 <zackary12389@yahoo.co.uk>
2020-10-29 20:07:14 +0000

    Created stats actions

--- client/src/store/modules/stats/actions.js
+++ client/src/store/modules/stats/actions.js
@@ -1,27 +1,45 @@
 import router from '@/router/index.js';
 
-const STATS_URL = 'http://localhost:5000/api/v1/stats/all';
+const STATS_URL = 'http://192.168.1.131:5000/api/v1/stats/all';
+const ALTV_URL = 'http://api.altv.mp/servers/list';
 
 export default {
     // Get All Stats
     getAll(context, payload) {
-        fetch(STATS_URL, {
-            method: 'GET',
-            headers: {
-                'content-type': 'application/json',
-                Authorization: `JWT ${localStorage.token}`,
-            },
-        })
-            .then(response => {
-                if (response.ok) {
-                    return response.json();
-                }
-                return response.json().then(error => {
-                    console.log(error);
-                });
+        if (this.getters['auth/isAdmin']) {
+            fetch(STATS_URL, {
+                method: 'GET',
+                headers: {
+                    'content-type': 'application/json',
+                    Authorization: `JWT ${localStorage.token}`,
+                },
             })
-            .then(result => {
-                context.commit('APP_SET_STATS', result);
-            });
+                .then(response => {
+                    if (response.ok) {
+                        return response.json();
+                    }
+                })
+                .then(result => {
+                    context.commit('APP_SET_STATS', result);
+                });
+        }
+    },
+
+    getAltV(context, payload) {
+        if (this.getters['auth/isAdmin']) {
+            fetch(ALTV_URL, {
+                method: 'GET',
+                headers: { 'content-Type': 'application/json' },
+            })
+                .then(res => {
+                    res.json().then(json => {
+                        const rebelServer = json.find(server => server.host == '185.254.99.20');
+                        context.commit('APP_SET_ALTV_SERVER', rebelServer);
+                    });
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
     },
 };
