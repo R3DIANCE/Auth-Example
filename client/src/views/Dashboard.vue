f0819367b5718e111b21e10f386f9fd4eeb6a4db
ZackaryH8 <zackary12389@yahoo.co.uk>
2020-10-29 20:04:28 +0000

    Added meta

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -2,12 +2,16 @@
     <div id="dasboard">
         <h1>Dashboard</h1>
 
-        <div class="centered">
+        <div class="card-container centered" v-if="isAdmin">
             <section class="cards">
                 <article class="card">
-                    Users
+                    Online Users
                     <br />
-                    {{ stats.userCount }}
+                    <span>{{ stats.altVServer.players }} / {{ stats.altVServer.maxPlayers }}</span>
+                    <div class="circle x-small" :data-fill="this.usersOnlinePercentage" hour style="--color: #d12727">
+                        <span>{{ usersOnlinePercentage }}%</span>
+                        <div class="bar"></div>
+                    </div>
                 </article>
 
                 <article class="card">
@@ -22,7 +26,7 @@
                     0
                 </article>
 
-                <article class="card">
+                <article class="card last-card">
                     Placeholder
                     <br />
                     0
@@ -33,15 +37,25 @@
         <p>{{ auth.user.username }}</p>
         <p>{{ auth.user._id }}</p>
 
+        <br />
+
+        <p>{{ stats.altVServer.branch }}</p>
+        <p>{{ stats.altVServer.build }}</p>
+
+        <br />
+
         <button class="button" @click="logout">Logout</button>
     </div>
 </template>
 
 <script>
-import { mapState } from 'vuex';
+import { mapState, mapGetters } from 'vuex';
 
 export default {
     name: 'Dashboard',
+    metaInfo: {
+        title: `Rebel Roleplay - Dashboard`,
+    },
 
     methods: {
         logout() {
@@ -53,32 +67,84 @@ export default {
 
     computed: {
         ...mapState(['auth', 'stats']),
+        ...mapGetters({
+            isAdmin: 'auth/isAdmin',
+        }),
+
+        usersOnlinePercentage() {
+            if (this.stats.altVServer) {
+                let percentage = Math.round((this.stats.altVServer.players / this.stats.altVServer.maxPlayers) * 100);
+                if (percentage == 51) {
+                    percentage = 50;
+                }
+                return percentage || 0;
+            }
+        },
     },
 
     mounted() {
-        this.$store.dispatch('auth/loggedIn', localStorage.token);
-        this.$store.dispatch('stats/getAll', localStorage.token);
+        this.$store.dispatch('auth/loggedIn', { token: localStorage.token, getStats: true });
+        this.$store.dispatch('stats/getAltV');
     },
 };
 </script>
 
-<style scoped>
-#home {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+<style lang="less" scoped>
+@import '../assets/css/circle.less';
+
+#dasboard {
+    padding: 15px;
+}
+
+.card-container {
+    margin-top: 10px;
 }
 
 .cards {
     display: flex;
     justify-content: space-between;
-    background-color: #282828;
     margin-bottom: 20px;
+    user-select: none;
 }
+
 .card {
-    margin: 5px;
+    display: flex;
+    flex-direction: column;
+    background-color: #2f2f2f;
     padding: 15px;
+    margin: 0px 10px 0px 0px;
     width: 100%;
+    border-radius: 3px;
+    cursor: pointer;
+}
+
+.last-card {
+    margin: 0px;
+}
+
+.circle {
+    align-self: flex-end;
+}
+
+@media only screen and (max-width: 600px) {
+    .cards {
+        flex-wrap: wrap;
+    }
+    .card {
+        flex-basis: calc(50% - 10px);
+        margin: 3px;
+        height: 120px;
+    }
+}
+
+@media only screen and (max-width: 380px) {
+    .cards {
+        flex-wrap: nowrap;
+        flex-direction: column;
+    }
+    .card {
+        flex-basis: 0;
+        height: 120px;
+    }
 }
 </style>
