c33a3dca11b3fc7608a04ca816abdbbf7a970810
ZackaryH8 <zackary12389@yahoo.co.uk>
2020-10-29 20:09:07 +0000

    Created mutators and added error checking

--- client/src/store/modules/stats/mutations.js
+++ client/src/store/modules/stats/mutations.js
@@ -1,6 +1,15 @@
 export default {
     APP_SET_STATS(state, payload) {
-        state.userCount = payload.userCount;
-        state.adminCount = payload.adminCount;
+        if (payload) {
+            state.userCount = payload.userCount;
+            state.adminCount = payload.adminCount;
+        }
+    },
+    APP_SET_ALTV_SERVER(state, payload) {
+        state.altVServer = payload;
+    },
+    APP_CLEAR_STATE(state, payload) {
+        state.userCount = 0;
+        state.adminCount = 0;
     },
 };
